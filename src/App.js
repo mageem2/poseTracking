e2d8cd01f6ff586f2c68b1aa1177cae0b0a52bfb
Benthediver99 <raderb@uindy.edu>
2022-02-19 17:57:04 -0500

    some import fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, Dimensions, Platform, TouchableOpacity, Button } from 'react-native';
+import { View } from 'react-native';
 import {PoseClassifier} from './PoseClassifier.js';
 
 export default function App() {

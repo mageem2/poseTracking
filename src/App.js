5fc3d5483be7817fe772a8f5859304d9c3a6af7a
Benthediver99 <raderb@uindy.edu>
2022-02-22 23:01:15 -0500

    Starting Example App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { View } from 'react-native';
-import PoseClassifier from './PoseClassifier';
+import MainMenu from './MainMenu';
+
 
 export default function App() {
   return (
     <View>
-      <PoseClassifier/>
+      <MainMenu/>
     </View>
   );
 }
---
+++ src/MainMenu.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { View } from 'react-native';
+import PoseExample from './PoseExample';
+
+export default function MainMenu() {
+
+    const [atMenu, setAtMenu] = setState(true);
+    const [targetPose, setTargetPose] = setState('');
+    
+    const handlePoseButtonPress = (poseFromButtom) ⇒ {
+        setTargetPose(poseFromButton);
+    }
+    
+    if(atMenu) {
+        return(
+            <TouchableOpacity
+                style={styles.poseButton}
+                onPress={('pose_1') ⇒ handlePoseButtonPress}
+            >
+                <Text>Pose 1<Text/>
+            <TouchableOpacity>
+            <TouchableOpacity
+                style={styles.poseButton}
+                onPress={('pose_2') ⇒ handlePoseButtonPress}
+            >
+                <Text>Pose 2<Text/>
+            <TouchableOpacity>
+            <TouchableOpacity
+                style={styles.poseButton}
+                onPress={('pose_3') ⇒ handlePoseButtonPress}
+            >
+                <Text>Pose 3<Text/>
+            <TouchableOpacity>
+            <TouchableOpacity
+                style={styles.poseButton}
+                onPress={('pose_4') ⇒ handlePoseButtonPress}
+            >
+                <Text>Pose 4<Text/>
+            <TouchableOpacity>
+        );
+    } else {
+        return (
+            <PoseExample
+            //props or inputs
+                targetPoseInput={targetPose}
+                style={styles.poseScreen}
+                backButton
+            ></PoseExample>
+        );
+    }
+}

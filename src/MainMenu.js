3f7a30334800154897367604438869c59416e601
Benthediver99 <raderb@uindy.edu>
2022-02-22 23:16:34 -0500

    more Main Menu changes

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,49 +1,53 @@
-
+import React, { useEffect, useState, useRef } from 'react';
+import { View , TouchableOpacity} from 'react-native';
+import PoseExample from './PoseExample';
 
 export default function MainMenu() {
-	const [atMenu, setAtMenu] = setState(true);
-	const [targetPose, setTargetPose] = setState('');
-	
-	const handlePoseButtonPress = (poseFromButtom) ⇒ {
-		setTargetPose(poseFromButton);
-	}
-	
-	if(atMenu){
-		return(
-			<TouchableOpacity
-				style={styles.poseButton}
-				onPress={('pose_1') ⇒ handlePoseButtonPress}
-			>
-				<Text>Pose 1<Text/>
-			<TouchableOpacity>
-			<TouchableOpacity
-				style={styles.poseButton}
-				onPress={('pose_2') ⇒ handlePoseButtonPress}
-			>
-				<Text>Pose 2<Text/>
-			<TouchableOpacity>
-			<TouchableOpacity
-				style={styles.poseButton}
-				onPress={('pose_3') ⇒ handlePoseButtonPress}
-			>
-				<Text>Pose 3<Text/>
-			<TouchableOpacity>
-			<TouchableOpacity
-				style={styles.poseButton}
-				onPress={('pose_4') ⇒ handlePoseButtonPress}
-			>
-				<Text>Pose 4<Text/>
-			<TouchableOpacity>
-			}
-		);
-	}else {
-		return (
-			<PoseExample
-			//props or inputs
-				targetPose={poseStateVariable}
-				style={styles.poseScreen}
-				backButton
-			></PoseExample>
-		);
-	}
+
+    const [atMenu, setAtMenu] = setState(true);
+    const [targetPose, setTargetPose] = setState('');
+    
+    const handlePoseButtonPress = (poseFromButtom) => {
+        setTargetPose(poseFromButton);
+    }
+    
+    if(atMenu) {
+        return(
+            <View>
+                <TouchableOpacity
+                    style={styles.poseButton}
+                    onPress={('pose_1') => {handlePoseButtonPress}}
+                >
+                    <Text>Pose 1</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.poseButton}
+                    onPress={('pose_2') =>{handlePoseButtonPress}}
+                >
+                    <Text>Pose 2</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.poseButton}
+                    onPress={('pose_3') => {handlePoseButtonPress}}
+                >
+                    <Text>Pose 3</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.poseButton}
+                    onPress={('pose_4') => {handlePoseButtonPress}}
+                >
+                    <Text>Pose 4</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    } else {
+        return (
+            <PoseExample
+            //props or inputs
+                targetPoseInput={targetPose}
+                style={styles.poseScreen}
+                backButton
+            ></PoseExample>
+        );
+    }
 }

6a827ff9847c9e40bd06564dbee79f0239037bf3
Benthediver99 <raderb@uindy.edu>
2022-03-29 13:24:02 -0400

    Exercise Classification V1.0.0 - Pushups & Tree-to-T

--- src/ClassificationUtil.js
+++ src/ClassificationUtil.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
-import { fetch ,asyncStorageIO, bundleResourceIO, decodeJpeg} from '@tensorflow/tfjs-react-native';
+import { fetch, asyncStorageIO, bundleResourceIO, decodeJpeg } from '@tensorflow/tfjs-react-native';
 import { and } from 'react-native-reanimated';
 import { encode } from 'punycode';
-export default class ClassificationUtil{
+export default class ClassificationUtil {
 
     //Sets up stats hooks for the classificatiion util class
     //These can be used throughout the below functions as
     //if they were global variables
-    constructor(){
-        this.model=null;
-        this.model_classes=null;
+    constructor() {
+        this.model = null;
+        this.model_classes = null;
         this.loadClassification.bind(this);
         this.classifyPose.bind(this);
         this.classifyPoses.bind(this);
@@ -19,26 +19,26 @@ export default class ClassificationUtil{
         this.getClassifiedEncodedPose.bind(this);
         this.classifyExercise.bind(this);
         this.trackMovement.bind(this);
-        this.model_url=null;
-        this.pose_map=null;
-        this.exercise_map=null;
-        this.exercise_name_map=null;
-        this.exercise_trie=null;  
-        this.movement_window=[];  //arramovement_window
-        this.classified_pose=null;  //string
-        this.classified_exercise=null;  //string
-        this.classified_exercises=null; //object (JSON)
+        this.model_url = null;
+        this.pose_map = null;
+        this.exercise_map = null;
+        this.exercise_name_map = null;
+        this.exercise_trie = null;
+        this.movement_window = [];  //arramovement_window
+        this.classified_pose = null;  //string
+        this.classified_exercise = null;  //string
+        this.classified_exercises = null; //object (JSON)
 
         //TODO Fixes or temporary
-        this.framecounter=0;
-        this.resetlimit=20;
-        this.sameposecounter=0;
-        this.smoothingbuffer=1;
+        this.framecounter = 0;
+        this.resetlimit = 20;
+        this.sameposecounter = 0;
+        this.smoothingbuffer = 1;
     }
 
     //'loadClassification'
-    async loadClassification(model_url){
-        
+    async loadClassification(model_url) {
+
         //Make sure tf is ready
         await tf.ready();
 
@@ -48,8 +48,8 @@ export default class ClassificationUtil{
             this.model_classes = require('./assets/classes.json');
             console.log("Loaded Tensor Server Model");
 
-        //If server-based doesn't work, then load the statically bundled model
-        //from within the source code
+            //If server-based doesn't work, then load the statically bundled model
+            //from within the source code
         }
         catch {
             const modelJSON = require('./assets/model.json');
@@ -61,8 +61,8 @@ export default class ClassificationUtil{
         }
 
         console.log(this.model);
-        console.log("Known Poses: ",this.model_classes);
-        
+        console.log("Known Poses: ", this.model_classes);
+
         //Create UTF-16 Encoded Pose Map 
         //------------------------------------------------------
         //-accounts for surrogate pairs
@@ -73,9 +73,9 @@ export default class ClassificationUtil{
         function convertToHex(str) {
             var hex;
             var result = "";
-            for (let i=0; i<str.length; i++) {
+            for (let i = 0; i < str.length; i++) {
                 hex = str.charCodeAt(i).toString(16);
-                result += ("000"+hex).slice(-4);
+                result += ("000" + hex).slice(-4);
             }
             result = "0x" + result;
             return result
@@ -95,11 +95,11 @@ export default class ClassificationUtil{
 
         //For loop to create pose map from classes.json pose names
         this.pose_map = {};  //JSON object that will act like a dictionary
-                             //for poses and their associated UTF-16 encoding
+        //for poses and their associated UTF-16 encoding
         let num_poses = this.model_classes.length;
         let current_num = '';
         for (let i = 0; i < num_poses; i++) {  //creates a valid encoding for each
-                                               //known pose in classes.json
+            //known pose in classes.json
             current_num = '' + i;
             let code_point = convertToHex(current_num);
             let char = String.fromCodePoint(code_point);
@@ -110,7 +110,7 @@ export default class ClassificationUtil{
                 i--;
             }
         }
-        console.log("Pose Map: ",this.pose_map);
+        console.log("Pose Map: ", this.pose_map);
 
         //---------------------END------------------------------
 
@@ -123,13 +123,13 @@ export default class ClassificationUtil{
         for (var exercise in exercises) {
             let encoded_exercise_string = "";
             let pose_name = "";
-            for (let i = 0; i < exercises[exercise].length; i++){
+            for (let i = 0; i < exercises[exercise].length; i++) {
                 pose_name = exercises[exercise][i];
                 encoded_exercise_string += this.pose_map[pose_name];
             }
             this.exercise_map[encoded_exercise_string] = exercise;
         }
-        console.log("Exercise Map: ",this.exercise_map);
+        console.log("Exercise Map: ", this.exercise_map);
 
         //---------------------END------------------------------
 
@@ -149,12 +149,12 @@ export default class ClassificationUtil{
 
         //Create Classified Exercises Variable
         //------------------------------------------------------
-        this.classified_exercises={};
+        this.classified_exercises = {};
         for (var exercise in this.exercise_map) {
             var exercise_name = this.exercise_map[exercise];
-            this.classified_exercises[exercise_name]=0;
+            this.classified_exercises[exercise_name] = 0;
         }
-        console.log("Classified Exercises: ",this.classified_exercises);
+        console.log("Classified Exercises: ", this.classified_exercises);
         //---------------------END------------------------------
         //Create Exercise Encoding Map
         //------------------------------------------------------
@@ -169,17 +169,17 @@ export default class ClassificationUtil{
     }
 
     // 'classifyPose'
-    async classifyPose (keypoints) { 
+    async classifyPose(keypoints) {
 
         //use model to predict
         const array = this.formatArray(keypoints);
         const tensor_of_keypoints = tf.tensor(array);
 
         //If the model exists then do classification
-        if(this.model){
+        if (this.model) {
             const predictionTensor = await this.model.predict(tensor_of_keypoints);
-            const [poseName, confidence] = await this.getClassifiedPose(predictionTensor,this.model_classes);
-            
+            const [poseName, confidence] = await this.getClassifiedPose(predictionTensor, this.model_classes);
+
             this.classified_pose = poseName;  //utilized with movement tracking / exercise classification
 
             return [poseName, confidence];
@@ -187,15 +187,15 @@ export default class ClassificationUtil{
     }
 
     // 'classifyPoses'
-    async classifyPoses (keypoints) { 
+    async classifyPoses(keypoints) {
         //use model to predict
         const array = this.formatArray(keypoints);
         const tensor_of_keypoints = tf.tensor(array);
 
         //If the model exists then do classification
-        if(this.model) {
+        if (this.model) {
             const predictionTensor = await this.model.predict(tensor_of_keypoints);
-            const classifiedPoses = await this.getClassifiedPoses(predictionTensor,this.model_classes, this.model_classes.length);
+            const classifiedPoses = await this.getClassifiedPoses(predictionTensor, this.model_classes, this.model_classes.length);
 
             const poseName = classifiedPoses.classifiedPoses[0].poseName;  //gets the highest confidence pose name from poses object
             this.classified_pose = poseName;  //utilized with movement tracking / exercise classification
@@ -229,8 +229,8 @@ export default class ClassificationUtil{
     // highest confidence.
     // Topk() from tf.js returns the value
     // and index with the highest value/confidence.
-    async getClassifiedPose (prediction, classes) {
-        const {values, indices} = await prediction.topk();
+    async getClassifiedPose(prediction, classes) {
+        const { values, indices } = await prediction.topk();
         const topKValues = await values.data();
         const topKIndices = await indices.data();
 
@@ -245,18 +245,18 @@ export default class ClassificationUtil{
     //- Topk (when given number of inputs) gives back sorted values and indices
     //- This function essentially returns the pose names and confidences
     //  that are detected with each frame.
-    async getClassifiedPoses (prediction, classes, numPoses) {
-        const {values, indices} = await prediction.topk(numPoses);
+    async getClassifiedPoses(prediction, classes, numPoses) {
+        const { values, indices } = await prediction.topk(numPoses);
         const topKValues = await values.data();
         const topKIndices = await indices.data();
 
-        let posesObject = {classifiedPoses : []}; //This will store an array of pose objects
-                                                    //each with a name & confidence
+        let posesObject = { classifiedPoses: [] }; //This will store an array of pose objects
+        //each with a name & confidence
         for (let i = 0; i < topKIndices.length; i++) {
-            let tempPoseObject = {poseName:"", confidence: 0.00};  //Maybe add encoding here: 
-                                                                   //", encoding: this.exercise_map['']""
-                                                                   //tempPoseObject.encoding = 
-                                                                   // this.exercise_map[classes[topKIndices[i]]];
+            let tempPoseObject = { poseName: "", confidence: 0.00 };  //Maybe add encoding here: 
+            //", encoding: this.exercise_map['']""
+            //tempPoseObject.encoding = 
+            // this.exercise_map[classes[topKIndices[i]]];
             tempPoseObject.poseName = classes[topKIndices[i]];
             tempPoseObject.confidence = topKValues[i];
             posesObject.classifiedPoses.push(tempPoseObject);
@@ -283,14 +283,14 @@ export default class ClassificationUtil{
     }
 
     //Simply returns the encoding a pose
-    async getClassifiedEncodedPose (poseName) {
-        if(poseName) {
+    async getClassifiedEncodedPose(poseName) {
+        if (poseName) {
             var encoded_pose = this.pose_map[poseName]
-            if(encoded_pose) {return encoded_pose;}
-            else {return "";}
+            if (encoded_pose) { return encoded_pose; }
+            else { return ""; }
         } else {
             return "";
-        } 
+        }
     }
 
     //'trackMovement'
@@ -298,12 +298,12 @@ export default class ClassificationUtil{
     // check the current classifiedPose and add it
     // to the movement window to be used with
     // exercise classification
-    async trackMovement () {
-        if(this.framecounter > this.resetlimit) {
+    async trackMovement() {
+        if (this.framecounter > this.resetlimit) {
             this.movement_window = []; //empties the exercise tracker
-                                        //if the user has been doing the same
-                                        //pose for too long (20 frames)
-            this.framecounter=0; //resets framecount
+            //if the user has been doing the same
+            //pose for too long (20 frames)
+            this.framecounter = 0; //resets framecount
         }
         try {  //makes sure a pose has been classified
             var pose_name = this.classified_pose;
@@ -312,30 +312,30 @@ export default class ClassificationUtil{
         }
         let encoded_pose = await this.getClassifiedEncodedPose(pose_name);
         let has_pose = this.exercise_trie.has(encoded_pose); //looks at if pose exists in known
-                                                              //exercises trie.
-                                                              //undefined - the prefix exists
-                                                              //true - the exact word exists
-                                                              //false - the prefix or word doesn't exist
-        if(this.movement_window.length > 0) {  //if movement window is not empty
+        //exercises trie.
+        //undefined - the prefix exists
+        //true - the exact word exists
+        //false - the prefix or word doesn't exist
+        if (this.movement_window.length > 0) {  //if movement window is not empty
             let temp_window = [...this.movement_window]; //copies movement window array
             temp_window.push(encoded_pose);
             let movement_window_with_new_encoded_pose = temp_window;
             let encoded_prefix = movement_window_with_new_encoded_pose.join("");
             let has_prefix = this.exercise_trie.has(encoded_prefix); //looks at if prefix exists in known
-                                                                //exercises trie.
-                                                                //undefined - the prefix exists
-                                                                //true - the exact word exists
-                                                                //false - the prefix or word doesn't exist
+            //exercises trie.
+            //undefined - the prefix exists
+            //true - the exact word exists
+            //false - the prefix or word doesn't exist
             var previous_pose = this.movement_window[this.movement_window.length - 1];
-            if(previous_pose!=encoded_pose) {//a new pose has been detected
-                if(has_prefix==undefined || has_prefix==true) {//pose exists in an exercise
-                    if(this.sameposecounter > this.smoothingbuffer) {//makes sure user is not going between poses way too fast
-                                                    //   because of close confidences between two poses
-                                                    //  . Simply adds a little buffer to that situation.
-                                                    // - A synthetic way of doing smoothing.
+            if (previous_pose != encoded_pose) {//a new pose has been detected
+                if (has_prefix == undefined || has_prefix == true) {//pose exists in an exercise
+                    if (this.sameposecounter > this.smoothingbuffer) {//makes sure user is not going between poses way too fast
+                        //   because of close confidences between two poses
+                        //  . Simply adds a little buffer to that situation.
+                        // - A synthetic way of doing smoothing.
                         this.movement_window.push(encoded_pose);
-                        this.sameposecounter=0;
-                        this.framecounter=0;
+                        this.sameposecounter = 0;
+                        this.framecounter = 0;
                     } else {// Pose changed too fast.  Increment counters to assume same pose was done
                         this.sameposecounter++;
                         this.framecounter++;
@@ -348,29 +348,29 @@ export default class ClassificationUtil{
                 this.framecounter++;
             }
         } else {  //movement window is empty
-            if(has_pose==undefined) {
+            if (has_pose == undefined) {
                 this.movement_window.push(encoded_pose);
             }
             this.framecounter++;
         }
-        console.log("Movement Window: ",this.movement_window);
+        console.log("Movement Window: ", this.movement_window);
     }
 
-    async classifyExercise () {
-        if (!this.movement_window) {return;}
+    async classifyExercise() {
+        if (!this.movement_window) { return; }
         var movement_string = this.movement_window.join("");
         var max_distance = 1;
-        var results = this.exercise_trie.find(movement_string,max_distance);
-        for(var prefix in results) {//Look at each possible exercise from the current movement window.
+        var results = this.exercise_trie.find(movement_string, max_distance);
+        for (var prefix in results) {//Look at each possible exercise from the current movement window.
             const distance = results[prefix];
-            if(distance == 0) {     //movement window is a known exercise
+            if (distance == 0) {     //movement window is a known exercise
                 const classified_exercise_name = this.exercise_map[prefix];
                 this.classified_exercise = classified_exercise_name;      //Save this classified exercise to
-                                                                          //the state variable to keep track
-                                                                          //of the most recent exercise.
+                //the state variable to keep track
+                //of the most recent exercise.
                 this.classified_exercises[classified_exercise_name] += 1; //Add a rep to classifed exercise
-                                                                          //to classified exercise object.
-                                                                          //This allows for rep tracking.
+                //to classified exercise object.
+                //This allows for rep tracking.
                 this.movement_window = [];  //empties current movement window
             }
         }
@@ -381,7 +381,7 @@ export default class ClassificationUtil{
     // 'formatArray' takes a 2d array of 33 pose keypoints/landmarks
     // and converts it to 99 separate points of data by separating
     // the x, y, and z points of data.  (33*3=99)
-    formatArray(pose){
+    formatArray(pose) {
         let arr_expanded = new Array([])
         if (pose.length > 0) {
             //define a new array
